0ec6ce1daa9571d408869dcb7e83ee1676e0d6be
Gavin Levis <levis.gavin@gmail.com>
2024-05-02 07:57:14 -0500

    add operate function

--- calculator.js
+++ calculator.js
@@ -14,3 +14,21 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+var num1;
+var num2;
+var operator;
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            console.log("Invalid operator.");
+    }
+}
